cc382bb60f854d92e365a30d0be5e341486b542e
bkalika <bogdan.kalika@gmail.com>
2020-06-04 21:39:01 +0300

    lesson 13

---
+++ css_skill_up/13_js/lesson_13.js
@@ -0,0 +1,13 @@
+let currentYear = +prompt("What year is now?","");
+(currentYear === 2020) ? alert("Now 2020 y!") : alert("Now " + currentYear)
+запитати пору року,
+української мовою вводить -> весна Весна
+липень серпень вересень
+
+let currentMonth = prompt("What month is now?", "");
+
+if currentMonth in ("january", "february", "december") {
+    alert("Now is winter")
+}else{
+    alert("now is spring")
+}
