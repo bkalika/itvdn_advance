806cada720b024a540a59198f9126da780bda9bd
bkalika <bogdan.kalika@gmail.com>
2020-07-14 23:02:58 +0300

    add productCount

---
+++ css_skill_up/VitalikTR.github.io-master/app.js
@@ -0,0 +1,116 @@
+// add to cart
+let productsCountEl = document.getElementById("products-count");
+// console.log(productsCountEl);
+
+let addToCartsButtons = document.querySelectorAll(".add-cart");
+// console.log(addToCartsButtons);
+    for (let i=0; i < addToCartsButtons.length; i++) {
+        addToCartsButtons[i].addEventListener("click", function() {
+            let prevProductsCount = +productsCountEl.textContent;
+                productsCountEl.textContent = prevProductsCount + 1;
+        
+    })
+}
+
+// change like start
+let likeButtons = document.querySelectorAll(".image-heart");
+// console.log(likeButtons);
+    for (let i=0; i < likeButtons.length; i++) {   
+        likeButtons[i].addEventListener("click", function() {
+            // this.classList.toggle("licked");
+            if (this.classList.contains("licked")) {
+                this.classList.remove("licked")
+                } else {
+                         this.classList.add("licked")
+            }
+        })
+    }
+
+
+    // modal
+
+    let moreDetailsButtons = document.querySelectorAll(".more-details");
+    // console.log(moredetailsButtons);
+    let modal = document.querySelector(".modal")
+    let closeBtn = document.querySelector(".btn-close")
+    moreDetailsButtons.forEach(function(btn){
+        btn.addEventListener("click", function(){
+            modal.classList.add("show")
+                modal.classList.remove("hide")
+        })
+    })
+
+    function openModal() {
+        modal.classList.add("show")
+            modal.classList.remove("hide")
+    }
+
+    function closeModal() {
+        modal.classList.add("hide")
+            modal.classList.remove("show")
+    }
+
+    function openModalByScroll() {
+        if(window.pageYOffset >= document.documentElement.scrollHeight/2) {
+            openModal()
+            window.removeEventListener('scroll', openModalByScroll)
+        }
+    }
+
+    window.addEventListener('scroll', openModalByScroll)
+
+    closeBtn.addEventListener("click",closeModal);
+        modal.addEventListener("click", function(e){
+            if (e.target === modal) {
+                closeModal()
+        }
+    })
+
+        // slider
+
+        // $('.slider-block').slick()
+        
+        let swiper = new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+          });
+
+// change product quantity
+let decrease = document.querySelectorAll('.decrement-btn')[0],
+    increase = document.querySelectorAll('.increment-btn')[0],
+    productCount = document.querySelectorAll('.product-quantity input')[0];
+    let currentCount = productCount.value;
+
+function toggleButtonState(count){
+    if(count <= 1){
+        decrease.disabled = true;
+    } else{
+        decrease.disabled = false;
+    }
+}
+toggleButtonState(currentCount)
+
+increase.addEventListener('click', function(){
+    let currentCount = +productCount.value;
+    let nextCount = currentCount + 1;
+    productCount.value = nextCount;
+
+    toggleButtonState(nextCount)
+})
+
+decrease.addEventListener('click', function(){
+    let currentCount = +productCount.value;
+    let nextCount = currentCount - 1;
+    productCount.value = nextCount;
+
+    toggleButtonState(nextCount)
+})
